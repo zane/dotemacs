d04eb93b98f5dee99422194327bf03695cf9e149
Zane Shelby <zane@Zane-Shelbys-MacBook.local>
2011-01-12 18:49:15 -0800

    Moving elpa packages locally.

--- init.el
+++ init.el
@@ -1,5 +1,42 @@
+;; Turn off mouse interface early in startup to avoid momentary display
+;; You really don't need these; trust me.
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; Load path etc.
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
+(add-to-list 'load-path dotfiles-dir)
+
+;; Load up ELPA, the package manager
+
+(setq package-user-dir (concat dotfiles-dir "elpa"))
+
 (eval-after-load 'package
-  '(add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/")))
+  '(dolist (source '(("technomancy" . "http://repo.technomancy.us/emacs/")
+			    ("elpa" . "http://tromey.com/elpa/")))
+	    (add-to-list 'package-archives source t)))
+(package-initialize)
+(require 'zane-elpa)
+
+;; These should be loaded on startup rather than autoloaded on demand
+;; since they are likely to be used in every session
+
+(require 'cl)
+(require 'saveplace)
+(require 'ffap)
+(require 'uniquify)
+(require 'ansi-color)
+(require 'recentf)
+
+;; Autoload any customizations in the customizations directory 
+
+(mapc '(lambda (file)
+	 (eval-after-load )
+      (directory-files (concat dotfiles-dir "conf") nil "^.*el$")
+      
+;; Everything else
 
 (enable-theme 'tango-dark)
 
@@ -21,3 +58,4 @@
 (iswitchb-mode)
 
 (add-hook 'slime-repl-mode-hook 'clojure-mode-font-lock-setup)
+
---
+++ zane-elpa.el
@@ -0,0 +1,46 @@
+;;; zane-elpa.el --- Install a base set of packages automatically.
+;; 
+;; Based on starter-kit-elpa.el from the Emacs Starter Kit
+;; https://github.com/technomancy/emacs-starter-kit
+
+(require 'cl)
+
+(defvar zane-packages '(
+			css-mode
+			find-file-in-project
+			gist
+			idle-highlight
+			js2-mode
+			magit
+			paredit
+			)
+  "Libraries that should be installed by default.")
+
+(defun zane-elpa-install ()
+  "Install all zane packages that aren't installed."
+  (interactive)
+  (dolist (package zane-packages)
+    (unless (or (member package package-activated-list)
+                (functionp package))
+      (message "Installing %s" (symbol-name package))
+      (package-install package))))
+
+(defun esk-online? ()
+  "See if we're online.
+
+  Windows does not have the network-interface-list function, so we just have to assume it's online."
+  ;; TODO how could this work on Windows?
+  (if (and (functionp 'network-interface-list)
+           (network-interface-list))
+      (some (lambda (iface) (unless (equal "lo" (car iface))
+                         (member 'up (first (last (network-interface-info
+                                                   (car iface)))))))
+            (network-interface-list))
+    t))
+
+;; On your first run, this should pull in all the base packages.
+(when (esk-online?)
+  (unless package-archive-contents (package-refresh-contents))
+  (zane-elpa-install))
+
+(provide 'zane-elpa)
