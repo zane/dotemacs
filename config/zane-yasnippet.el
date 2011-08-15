811580eae55a44ab32e6e4201927781a09ff9f35
Zane Shelby <zaneshelby@gmail.com>
2011-08-15 12:28:27 -0500

    Initial commit.

---
+++ config/zane-yasnippet.el
@@ -0,0 +1,8 @@
+;; svn checkout http://yasnippet.googlecode.com/svn/trunk/ ~/Projects yasnippet.googlecode
+(add-to-list 'load-path (concat zane-emacs-root "../yasnippet.googlecode"))
+(require 'yasnippet)
+(eval-after-load "yasnippet"
+  '(progn
+     (yas/initialize)
+     ;(yas/load-directory (concat zane-emacs-root "..."))
+     ))
