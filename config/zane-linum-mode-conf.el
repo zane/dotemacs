595adb3243d600c22af8b5c6aca345862fa034ce
Zane Shelby <zaneshelby@gmail.com>
2011-08-15 12:21:50 -0500

    Don't add line numbers to fundamental-mode buffers.

--- config/zane-linum-mode-conf.el
+++ config/zane-linum-mode-conf.el
@@ -1,7 +1,5 @@
 (global-linum-mode 1)
 (fringe-mode -1)
-(add-to-list 'load-path (concat zane-emacs-root "lib"))
-(require 'linum-off)
 (setq linum-format
       (lambda (line)
         (propertize (format
@@ -10,3 +8,7 @@
                        (concat "%" (number-to-string w) "d "))
                      line)
                     'face 'linum)))
+
+(add-to-list 'load-path (concat zane-emacs-root "lib"))
+(require 'linum-off)
+(add-to-list 'linum-disabled-modes-list 'fundamental-mode)
