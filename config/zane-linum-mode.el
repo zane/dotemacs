979cad9ff3f7e6fdcf8163695fcbf844d65c4aff
Zane Shelby <zaneshelby@gmail.com>
2011-08-17 11:45:48 -0500

    Moving linum customizations into an eval-after-load.

--- config/zane-linum-mode.el
+++ config/zane-linum-mode.el
@@ -1,15 +1,20 @@
-(global-linum-mode 1)
-;; FIXME: This is broken... Figure out how to set this programmatically.
-(fringe-mode 'no-fringes)
-(setq linum-format
-      (lambda (line)
-        (propertize (format
-                     (let ((w (length (number-to-string
-                                       (count-lines (point-min) (point-max))))))
-                       (concat "%" (number-to-string w) "d "))
-                     line)
-                    'face 'linum)))
+(eval-after-load 'linum-mode
+  '(progn
+     (global-linum-mode 1)
+     ;; FIXME: This is broken... Figure out how to set this programmatically.
+     (fringe-mode 'no-fringes)
+     ;; Add a space between line numbers and file text.
+     (setq linum-format
+           (lambda (line)
+             (propertize (format
+                          (let ((w (length (number-to-string
+                                            (count-lines (point-min) (point-max))))))
+                            (concat "%" (number-to-string w) "d "))
+                          line)
+                         'face 'linum)))))
 
 (add-to-list 'load-path (concat zane-emacs-root "lib"))
 (require 'linum-off)
-(add-to-list 'linum-disabled-modes-list 'fundamental-mode)
+(eval-after-load 'linum-off
+  '(progn (add-to-list 'linum-disabled-modes-list 'fundamental-mode)
+          (add-to-list 'linum-disabled-modes-list 'fundamental-mode)))
