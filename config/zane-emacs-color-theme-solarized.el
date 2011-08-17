56b4fdc4e975c608bc4e3f60023c342e95076d76
Zane Shelby <zaneshelby@gmail.com>
2011-08-17 11:44:27 -0500

    Theme tweaks.

--- config/zane-emacs-color-theme-solarized.el
+++ config/zane-emacs-color-theme-solarized.el
@@ -1,6 +1,9 @@
 (add-to-list 'load-path (concat zane-emacs-root "../emacs-color-theme-solarized.github.sellout"))
-(require 'solarized-dark-theme)
-(require 'solarized-light-theme)
-(enable-theme 'solarized-dark)
+;;(require 'solarized-dark-theme)
+;;(enable-theme 'solarized-dark)
+
+;;(require 'solarized-light-theme)
+;;(enable-theme 'solarized-light)
+
 (show-paren-mode -1)
 (fringe-mode -1)
---
+++ config/zane-zenburn-emacs.el
@@ -0,0 +1,4 @@
+;; git clone https://github.com/bbatsov/zenburn-emacs.git
+(add-to-list 'load-path (concat zane-emacs-root "../zenburn-emacs.github.bbatsov"))
+(require 'color-theme-zenburn)
+(color-theme-zenburn)
