b985b468a91b9cb95f664cf1b5f351ea79fccb5f
Zane Shelby <zaneshelby@gmail.com>
2011-08-17 19:44:12 -0500

    Switching over to my fork of zenburn.

--- config/zane-zenburn-emacs.el
+++ config/zane-zenburn-emacs.el
@@ -1,4 +1,4 @@
-;; git clone https://github.com/bbatsov/zenburn-emacs.git
-(add-to-list 'load-path (concat zane-emacs-root "../zenburn-emacs.github.bbatsov"))
+;; git clone git@github.com:zane/zenburn-emacs.git ~/Projects/zenburn-emacs.github.zane
+(add-to-list 'load-path (concat zane-emacs-root "../zenburn-emacs.github.zane"))
 (require 'color-theme-zenburn)
 (color-theme-zenburn)
