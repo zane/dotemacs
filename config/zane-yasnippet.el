9886c4471860e1f52e9a42518ed9b9f0b33d3f7a
Zane Shelby <zaneshelby@gmail.com>
2011-08-18 23:04:46 -0500

    Correcting URL for checking out yasnippet.

--- config/zane-yasnippet.el
+++ config/zane-yasnippet.el
@@ -1,4 +1,4 @@
-;; svn checkout http://yasnippet.googlecode.com/svn/trunk/ ~/Projects yasnippet.googlecode
+;; svn checkout http://yasnippet.googlecode.com/svn/trunk/ ~/Projects/yasnippet.googlecode
 (add-to-list 'load-path (concat zane-emacs-root "../yasnippet.googlecode"))
 (require 'yasnippet)
 (eval-after-load "yasnippet"
