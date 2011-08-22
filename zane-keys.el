3896fbfcafa8816c3cce29d48cae6181c70d83a0
Zane Shelby <zaneshelby@gmail.com>
2011-08-22 12:00:03 -0500

    Delete trailing whitespace?

--- zane-keys.el
+++ zane-keys.el
@@ -5,8 +5,8 @@
 (setq mac-command-modifier 'meta)
 (global-set-key (kbd "C-c C-a") 'align-regexp)
 (global-set-key (kbd "C-c C-o") 'sort-lines)
-(global-set-key (kbd "C-x y") 'bury-buffer)
 (global-set-key (kbd "C-o")  'other-window)
+(global-set-key (kbd "C-x y") 'bury-buffer)
 (global-set-key (kbd "M-RET") 'ns-toggle-fullscreen) ; http://www.stratospark.com/blog/2010/fullscreen_emacs_on_osx.html
 (global-set-key (kbd "M-h") 'ns-do-hide-emacs)
 
