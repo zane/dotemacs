43d214453535c642695c2534499047d19b4c82e1
Zane Shelby <zaneshelby@gmail.com>
2011-08-23 10:53:56 -0500

    Switch over to Solarized.

--- config/zane-emacs-color-theme-solarized.el
+++ config/zane-emacs-color-theme-solarized.el
@@ -1,6 +1,7 @@
 (add-to-list 'load-path (concat zane-emacs-root "../emacs-color-theme-solarized.github.sellout"))
-;;(require 'solarized-dark-theme)
-;;(enable-theme 'solarized-dark)
+
+(require 'solarized-dark-theme)
+(enable-theme 'solarized-dark)
 
 ;;(require 'solarized-light-theme)
 ;;(enable-theme 'solarized-light)
--- config/zane-zenburn-emacs.el
+++ config/zane-zenburn-emacs.el
@@ -1,4 +1,4 @@
 ;; git clone git@github.com:zane/zenburn-emacs.git ~/Projects/zenburn-emacs.github.zane
 (add-to-list 'load-path (concat zane-emacs-root "../zenburn-emacs.github.zane"))
-(require 'color-theme-zenburn)
-(color-theme-zenburn)
+;; (require 'color-theme-zenburn)
+;; (color-theme-zenburn)
