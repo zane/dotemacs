457928386a788a8940b002cfa83ae81d0e38dd84
Zane Shelby <zaneshelby@gmail.com>
2011-08-24 14:59:20 -0500

    Adding flymake.

--- zane-el-get.el
+++ zane-el-get.el
@@ -67,6 +67,7 @@
                         (define-key dired-mode-map (kbd "C-r") 'dired-isearch-backward)
                         (define-key dired-mode-map (kbd "ESC C-s") 'dired-isearch-forward-regexp)
                         (define-key dired-mode-map (kbd "ESC C-r") 'dired-isearch-backward-regexp)))
+        (:name flymake-cursor :type emacswiki :features flymake-cursor)
         (:name rainbow-delimiters
                :type emacswiki
                :features rainbow-delimiters
