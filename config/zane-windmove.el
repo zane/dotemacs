e9f96daf8684dc5d18c1602a4ef4381ff4cbd864
Zane Shelby <zaneshelby@gmail.com>
2011-09-09 11:13:23 -0500

    Adding a TODO and some debugging to investigate why

--- config/zane-windmove.el
+++ config/zane-windmove.el
@@ -1,3 +1,5 @@
+;; TODO: Figure out why this isn't loding with init :(
+(message "zane-windmove:2 Is this loading?")
 (global-set-key (kbd "C-l") 'windmove-right)
 (global-set-key (kbd "C-j") 'windmove-left)
 (global-set-key (kbd "C-k") 'windmove-down)
