279a29f65ed684fa1422e7f4ee23c877309ff4bb
Zane Shelby <zaneshelby@gmail.com>
2011-09-20 09:41:14 -0500

    Don't use autocomplete with the shell.

--- zane-el-get.el
+++ zane-el-get.el
@@ -17,15 +17,11 @@
 (setq el-get-sources
       '((:name auto-complete
                :after (lambda ()
+                        (setq ac-auto-start nil)
                         (setq ac-sources (append ac-sources '(ac-source-yasnippet)))
                         (setq ac-use-menu-map t)
                         (define-key ac-menu-map (kbd "M-i") 'ac-previous)
                         (define-key ac-menu-map (kbd "M-k") 'ac-next)
-
-                        ;; Auto-complete for shell
-                        (add-to-list 'ac-modes 'shell-mode)
-                        (add-to-list 'shell-mode-hook
-                                     (lambda () (setq ac-sources '(ac-source-words-in-same-mode-buffers))))
 
                         ;; Auto-complete for text-files
                         (add-to-list 'ac-modes 'text-mode)
