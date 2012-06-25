16dad1c19b4dbe30ca922dc48422fe36b480169a
Zane Shelby <zaneshelby@gmail.com>
2012-06-25 12:02:22 -0500

    Remove unused zenburn config.

--- config/zane-zenburn-emacs.el
+++
@@ -1,4 +0,0 @@
-;; git clone git@github.com:zane/zenburn-emacs.git ~/Projects/zenburn-emacs.github.zane
-(add-to-list 'load-path (concat zane-projects-dir "zenburn-emacs.github.zane"))
-;; (require 'color-theme-zenburn)
-;; (color-theme-zenburn)
