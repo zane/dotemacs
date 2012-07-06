3ea1d46cfb6449c7aab4cfc88cb455d539578f1b
Zane Shelby <zaneshelby@gmail.com>
2012-07-06 13:35:44 -0500

    Prefix a few functions, add rotate windows.

--- zane-funcs.el
+++ zane-funcs.el
@@ -2,7 +2,7 @@
   "Truthy if the host OS is a Mac."
   (string-match "apple-darwin" system-configuration))
 
-(defun uniquify-all-lines-region (start end)
+(defun z:uniquify-all-lines-region (start end)
   "Find duplicate lines in region START to END keeping first occurrence."
   (interactive "*r")
   (save-excursion
@@ -13,24 +13,47 @@
             (re-search-forward "^\\(.*\\)\n\\(\\(.*\n\\)*\\)\\1\n" end t))
         (replace-match "\\1\n\\2")))))
 
-(defun uniquify-all-lines-buffer ()
+(defun z:uniquify-all-lines-buffer ()
   "Delete duplicate lines in buffer and keep first occurrence."
   (interactive "*")
-  (uniquify-all-lines-region (point-min) (point-max)))
+  (z:uniquify-all-lines-region (point-min) (point-max)))
 
-(defun set-transparency (value)
+(defun z:set-transparency (value)
   "Sets the transparency of the frame window. 0=transparent/100=opaque"
   (interactive "nTransparency Value 0 - 100 opaque:")
   (set-frame-parameter (selected-frame) 'alpha value))
 
-(defun switch-to-next-frame ()
+(defun z:switch-to-next-frame ()
   "Select the next frame on current display, and raise it."
   (interactive)
   (other-frame 1))
 
-(defun switch-to-previous-frame ()
+(defun z:switch-to-previous-frame ()
   "Select the previous frame on current display, and raise it."
   (interactive)
   (other-frame -1))
+
+;; http://emacswiki.org/emacs/TransposeWindows
+(defun z:rotate-windows ()
+  "Rotate your windows"
+  (interactive)
+  (cond
+   ((not (> (count-windows) 1))
+    (message "You can't rotate a single window!"))
+   (t
+    (let ((i 1)
+          (num-windows (count-windows)))
+      (while  (< i num-windows)
+        (let* ((w1 (elt (window-list) i))
+               (w2 (elt (window-list) (+ (% i num-windows) 1)))
+               (b1 (window-buffer w1))
+               (b2 (window-buffer w2))
+               (s1 (window-start w1))
+               (s2 (window-start w2)))
+          (set-window-buffer w1 b2)
+          (set-window-buffer w2 b1)
+          (set-window-start w1 s2)
+          (set-window-start w2 s1)
+          (setq i (1+ i))))))))
 
 (provide 'zane-funcs)
