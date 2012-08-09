7efb33f9837616d0b052595210845bda3800fb81
Zane Shelby <zaneshelby@gmail.com>
2012-08-09 13:07:27 -0500

    Make ANSI colors be more solarized.

--- config/zane-ansi-term.el
+++ config/zane-ansi-term.el
@@ -12,3 +12,11 @@
 
 ;; http://www.faqs.org/faqs/unix-faq/shell/zsh/#ixzz0TyTJsHMq
 ;; http://www.masteringemacs.org/articles/2010/11/01/running-shells-in-emacs-overview/
+
+;; http://tapoueh.org/blog/2011/07/29-emacs-ansi-colors.html
+(require 'ansi-color)
+(setq ansi-color-names-vector
+      (vector (frame-parameter nil 'background-color)
+               "#586e75" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#93a1a1")
+      ansi-term-color-vector ansi-color-names-vector
+      ansi-color-map (ansi-color-make-color-map))
