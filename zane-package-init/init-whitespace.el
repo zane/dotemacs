852443a3194fbceff681f6c0b9c33b1b5760ffcc
Zane Shelby <zaneshelby@gmail.com>
2012-10-22 16:31:22 -0500

    Use whitespace-action instead of a hook.

--- zane-package-init/init-whitespace.el
+++ zane-package-init/init-whitespace.el
@@ -1,4 +1,5 @@
 ;; http://xahlee.blogspot.com/2009/08/how-to-use-and-setup-emacss-whitespace.html
 (setq whitespace-style '(face trailing tabs empty))
-(add-hook 'before-save-hook 'whitespace-cleanup)
+
+(setq whitespace-action '(auto-cleanup))
 (global-whitespace-mode 1)
