6d2d84064ae299255bb0c6010198d87873c5a6e2
Zane Shelby <zaneshelby@gmail.com>
2013-04-09 18:27:02 -0700

    Treat gemfiles as Ruby. Fix <RET> in Ruby.

--- zane-package-init/init-ruby-mode.el
+++ zane-package-init/init-ruby-mode.el
@@ -1,4 +1,7 @@
-(add-hook 'ruby-mode-hook
-          (lambda ()
-            (eval-after-load "ergoemacs-mode"
-              '(ergoemacs-local-set-key (kbd "RET") 'reindent-then-newline-and-indent))))
+(defun z:reset-ret ()
+  (ergoemacs-local-set-key (kbd "RET") 'reindent-then-newline-and-indent))
+
+(eval-after-load "ergoemacs-mode"
+  '(add-hook 'ruby-mode-hook 'z:reset-ret))
+
+(add-to-list 'auto-mode-alist `("Gemfile" . ruby-mode))
