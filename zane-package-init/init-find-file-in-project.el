3dea31397222f7543a41adde03fa0d900e8be186
Zane Shelby <zaneshelby@gmail.com>
2013-04-09 18:26:12 -0700

    Add configuration for ffip. Try ffip before find-file everywhere.

---
+++ zane-package-init/init-find-file-in-project.el
@@ -0,0 +1,14 @@
+(eval-after-load 'find-file-in-project
+  '(progn
+     (add-to-list 'ffip-patterns "*.erb")
+     (setq ffip-find-options "-not -regex \".*cache.*\"")
+
+     (defun z/ffip-or-find-file ()
+       "Try to ffip, but fall back to regular find file if not in the context of a project."
+       (interactive)
+       (condition-case ex
+           (find-file-in-project)
+         ('error (ido-find-file))))
+
+     (eval-after-load "ergoemacs-mode"
+       '(ergoemacs-global-set-key (kbd "C-o") 'z/ffip-or-find-file))))
