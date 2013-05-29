4e2490f983ff198efb2cbaf8f27eb958a52bfeba
Zane Shelby <zaneshelby@gmail.com>
2013-05-29 18:24:37 -0400

    Only setup mc-run-for-all for paredit commands if paredit acutally gets loaded.

--- zane-package-init/init-multiple-cursors.el
+++ zane-package-init/init-multiple-cursors.el
@@ -3,8 +3,9 @@
     (ergoemacs-global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
     (ergoemacs-global-set-key (kbd "C->") 'mc/mark-next-like-this)
     (ergoemacs-global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-    (ergoemacs-global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
+    (ergoemacs-global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)))
 
-  (dolist (command '(paredit-backward-delete
+(after 'multiple-cursors
+    (dolist (command '(paredit-backward-delete
                      paredit-forward-delete))
     (add-to-list 'mc/cmds-to-run-for-all command)))
