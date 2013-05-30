af1dbefbc08c51f1076d10e92438f78e7e9d8f9d
Zane Shelby <zaneshelby@gmail.com>
2013-05-30 00:50:33 -0400

    No longer visualize or clean up tailing whitespace using whitespace-cleanup.

--- zane-package-init/init-whitespace.el
+++ zane-package-init/init-whitespace.el
@@ -1,5 +1,5 @@
 ;; http://xahlee.blogspot.com/2009/08/how-to-use-and-setup-emacss-whitespace.html
-(setq whitespace-style '(face trailing tabs empty))
-
-(setq whitespace-action '(auto-cleanup))
-(global-whitespace-mode 1)
+(after 'whitespace
+  (setq whitespace-style '(face tabs empty))
+  (setq whitespace-action '(auto-cleanup warn-if-read-only))
+  (global-whitespace-mode 1))
