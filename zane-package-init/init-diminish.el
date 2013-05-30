aac50073b80bd5652e4feeafad3050e1a8889790
Zane Shelby <zaneshelby@gmail.com>
2013-05-29 23:48:40 -0400

    Simplify major modes too, while we're at it.

--- zane-package-init/init-diminish.el
+++ zane-package-init/init-diminish.el
@@ -1,4 +1,13 @@
 (after 'diminish-autoloads
+  (defmacro rename-modeline (package-name mode new-name)
+    `(eval-after-load ,package-name
+       '(defadvice ,mode (after rename-modeline activate)
+          (setq mode-name ,new-name))))
+
+  ;; http://whattheemacsd.com//appearance.el-01.html
+  (rename-modeline 'lisp-mode emacs-lisp-mode "ELisp")
+  (rename-modeline 'js js-mode "JS")
+
   (after 'paredit         (diminish 'paredit-mode           " Φ"))
   (after 'yasnippet       (diminish 'yas-minor-mode         " Υ"))
   (after 'ergoemacs-mode  (diminish 'ergoemacs-mode         " Ε"))
