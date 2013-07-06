709e68fec46be0e33e08d50c8f4782d39f2737c7
Zane Shelby <zaneshelby@gmail.com>
2013-07-05 21:10:00 -0400

    Improve trailing whitespace regex.

--- zane-package-init/init-whitespace.el
+++ zane-package-init/init-whitespace.el
@@ -1,6 +1,6 @@
 ;; http://xahlee.blogspot.com/2009/08/how-to-use-and-setup-emacss-whitespace.html
 (setq whitespace-trailing-regexp
-      "\\b.*?\\(\\(\t\\| \\|\xA0\\|\x8A0\\|\x920\\|\xE20\\|\xF20\\)+\\)$")
+      "\\S-.*?\\(\\(\t\\| \\|\xA0\\|\x8A0\\|\x920\\|\xE20\\|\xF20\\)+\\)$")
 (setq whitespace-style '(face tabs trailing empty))
 (setq whitespace-action '(auto-cleanup warn-if-read-only))
 (global-whitespace-mode 1)
