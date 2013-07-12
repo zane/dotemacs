e27c2e5f533086a75bc98da81b0fc7d5d9bc241e
Zane Shelby <zaneshelby@gmail.com>
2013-07-12 09:09:57 -0400

    'Variants are now 'Themes'.

--- zane-package-init/init-ergoemacs-mode.el
+++ zane-package-init/init-ergoemacs-mode.el
@@ -3,8 +3,8 @@
 (after 'ergoemacs-mode
   (setenv "ERGOEMACS_KEYBOARD_LAYOUT" "us")
 
-  (ergoemacs-defvariant zane
-    "Zane Shelby variant -- ALL key except <apps> and <menu> keys."
+  (ergoemacs-deftheme zane
+    "Zane Shelby theme -- ALL key except <apps> and <menu> keys."
     nil
     (ergoemacs-replace-key 'ergoemacs-smex-if-exists "M-a" "M-a")
     (ergoemacs-replace-key  'ergoemacs-cut-line-or-region "M-x" "✂ region")
@@ -12,11 +12,13 @@
     (ergoemacs-minor-key 'ido-minibuffer-setup-hook '(backward-char ido-prev-match-dir minor-mode-overriding-map-alist))
     (ergoemacs-minor-key 'ido-minibuffer-setup-hook '(previous-line ido-prev-match minor-mode-overriding-map-alist))
     (ergoemacs-minor-key 'ido-minibuffer-setup-hook '(next-line ido-next-match minor-mode-overriding-map-alist))
+    ;; "<M-backspace>" backward-kill-word
     (setq ergoemacs-variable-layout-tmp
           (remove-if (lambda (x) (or (string-match "<apps>" (car x))
-                                (string-match "<menu>" (car x))))
+				     (string-match "<menu>" (car x))
+				     (string-match "<M-backspace>" (car x))))
                      ergoemacs-variable-layout)))
-  (setq ergoemacs-variant "zane")
+  (setq ergoemacs-theme "zane")
   (ergoemacs-mode 1)
   
   (global-set-key (kbd "C-/") 'zane/toggle-identifier-case-word-at-point)
