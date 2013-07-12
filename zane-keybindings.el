4d7b4a7490bafdaca837051237959e9fc8c63797
Zane Shelby <zaneshelby@gmail.com>
2013-07-12 09:12:15 -0400

    Fix dired interaction with occur.

--- zane-keybindings.el
+++ zane-keybindings.el
@@ -79,8 +79,8 @@
     (key-chord-define-global "jk" 'ace-jump-buffer)))
 
 (after 'dired
-  (define-key dired-mode-map (kbd "C-o") 'other-window)
   (after 'dired-search-autoloads
+    (define-key dired-mode-map (kbd "C-o") nil)
     (define-key dired-mode-map (kbd "C-s") 'dired-isearch-forward)
     (define-key dired-mode-map (kbd "C-r") 'dired-isearch-backward)
     (define-key dired-mode-map (kbd "ESC C-s") 'dired-isearch-forward-regexp)
